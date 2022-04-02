62b485d71d0f156ad6ce1f50af6df2563fbba58e
Ravi Mallah <ravimallah8@gmail.com>
2022-04-02 17:27:51 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/nav.css"
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+
+            Hello
+        </div>
+    )
+}
+
+export default Navbar
